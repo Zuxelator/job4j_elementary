94edc85385351a2909e6710b290d5194491042a7
Zuxelator <zuxel87@mail.ru>
2021-03-14 16:55:12 +0300

    9.2. Checkstyle [#496948]

--- src/ru/job4j/Calculator.java
+++ src/ru/job4j/Calculator.java
@@ -11,13 +11,13 @@ public class Calculator {
         int four = 4;
         int five = 5;
 
-
-        int sixDivTwo = six /two;
-        int fiveMinusTwo = five -two;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
 
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
+
     }
 }
