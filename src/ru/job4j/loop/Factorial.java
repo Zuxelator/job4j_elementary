db96b38f5d1411a67863a94fc189b4bd5c76b7c9
Zuxelator <zuxel87@mail.ru>
2021-03-19 20:54:03 +0300

    5.2. Создать программу, вычисляющую факториал. [#496998]

--- src/ru/job4j/loop/Factorial.java
+++ src/ru/job4j/loop/Factorial.java
@@ -4,7 +4,7 @@ public class Factorial {
     public static int calc(int n) {
 
             int result = 1;
-            for (int i = n; i >= 1; i--) {
+            for (int i = 1; i <= n; i++) {
                 result *= i;
             }
             return result;
